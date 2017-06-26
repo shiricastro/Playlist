03bddad208655d2f8249f90a377c45fcfdedb4aa
shiri castro <shiricastro3@gmail.com>
2017-06-26 18:56:48 +0300

    add playlist page

--- script.js
+++ script.js
@@ -16,9 +16,70 @@
  */
 
 
-$.get('albums.html', function(data){
-   var data = data;
-   $('.playlist-container').html(data);
+
+/*new Promise(function(resolve){
+    get("albums.html","albums-container" );
+    resolve();
+})
+.then(function(){
+    return new Promise(function(resolve){
+        console.log($('button#hh'));
+        console.log($('button.add-unicode'));
+        $('button.add-unicode').each(function(i,el){
+            $(el).click(function(e){
+                e.preventDefault();
+                get("playlist.html","playlist-container" );
+                $('.albums-container').remove();
+                $('input.search').css({"display":"none"});
+                $('input.back-button').css({"display":"block"});
+            });
+        });                
+        resolve();         
+    });
+})
+.then(function(){
+    return new Promise(function(resolve){
+            
+                console.log("3");
+            resolve();
+        }); 
+});*/
+
+    
+
+function get(url, className){
+     $.get( url , function(data){
+       var container = $('<div>',{class:className}).appendTo('body');
+       container.html(data);
+    });   
+}
+
+ get("albums.html","albums-container");
+   
+
+
+$(document).ready(function(){
+        get("playlist.html","playlist-container" );
+     $("button.play-unicode").click(function(e){
+        $(".playlist-container").css({"display":"flex"});
+        $(".albums-container").css({"display":"none"});
+        $('input.back-button').css({"display":"block"});
+        $('input.search').css({"display":"none"});
+        toHomePage();
+    });      
 });
+
+function toHomePage(){
+   $('input.back-button').click(function(){
+        $(".playlist-container").css({"display":"none"});
+        $(".albums-container").css({"display":"flex"});
+        $('input.back-button').css({"display":"none"});
+        $('input.search').css({"display":"block"});
+   }); 
+}
+
+
+
+
 
 
