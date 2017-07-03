460a7d4dbe079afc384b52806d390dadfa63c17f
shiri castro <shiricastro3@gmail.com>
2017-07-03 17:19:19 +0300

    add js

--- script.js
+++ script.js
@@ -5,85 +5,188 @@
         <div>add ➕</div>
         <div>pause ❚❚ </div>
 
-    <svg xmlns:svg="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xmlns="http://www.w3.org/2000/svg" version="1.1">
-        <defs>
-            <path id="text_0_path" d="M 100 150 A 100 100 0 1 1 300 150"/>
-        </defs>   
-        <text text-anchor="middle">
-            <textPath xlink:href="#text_0_path" startOffset="50%">Test Text hvmvgjhmmjmb</textPath>
-        </text>
-    </svg>
  */
-
-
-
-/*new Promise(function(resolve){
-    get("albums.html","albums-container" );
-    resolve();
-})
-.then(function(){
-    return new Promise(function(resolve){
-        console.log($('button#hh'));
-        console.log($('button.add-unicode'));
-        $('button.add-unicode').each(function(i,el){
-            $(el).click(function(e){
-                e.preventDefault();
-                get("playlist.html","playlist-container" );
-                $('.albums-container').remove();
-                $('input.search').css({"display":"none"});
-                $('input.back-button').css({"display":"block"});
-            });
-        });                
-        resolve();         
+albums();
+function albums(){
+    $.get('http://localhost/playlist/public_html/api/playlist', function(data){
+        var albumsConatainer = $('<div>',{class:"albums-container"}).appendTo('body');
+        $(data.data).each(function(i,el){
+            var album = $('<picture>',{class:"album-pic", id:el.id}).appendTo(albumsConatainer);
+            $('<figcaption>',{
+                text:el.name
+            }).appendTo(album);
+            var editor = $('<div>',{class:"editor"}).appendTo(album);
+            $('<button>',{text:"✖",click:function(e){deleteAlbumPopup(e,null);}}).appendTo(editor);
+            $('<button>',{text:"✏",click:editAlbum}).appendTo(editor);
+            var picContainer = $('<div>',{class:"pic-container"}).appendTo(album);
+            $('<img>',{src: el.image,alt:el.name}).appendTo(picContainer);
+            var playContainer = $('<div>',{class:"play-container"}).appendTo(picContainer);
+            $('<button>',{text:"►", class:"play-unicode",click:function(e){playAlbum(e,el.id);}}).appendTo(playContainer);
+        });
     });
-})
-.then(function(){
-    return new Promise(function(resolve){
-            
-                console.log("3");
-            resolve();
-        }); 
-});*/
+}
 
+function playAlbum(e,id){
+    $('input.back-button').css({"display":"block"});
+    $('input.back-button').click(backHome);
+    $('input.search').css({"display":"none"});
+    $('.albums-container').remove();
+
+    $.get('playlist.html', function(playlistData){
+        let playlistConatainer = $('<div>',{class:"playlist-container"}).appendTo('body');
+        $('.playlist-editor button.deleteAlbumButton').click(function(e){deleteAlbumPopup(e,id);});
+        console.log($(playlistData).find('.playlist-editor button.deleteAlbumButton'));
+        $.get("" + 'http://localhost/playlist/public_html/api/playlist/' + id +"", function(data){
+            let dataAlbum = data.data;
+           $(playlistData).appendTo(playlistConatainer);
+           $(playlistConatainer).find($('span.name')).html(dataAlbum.name);
+           $(playlistConatainer).find($('div.pic-container img')).attr("src",dataAlbum.image);
+        });
+
+        $.get("" + 'http://localhost/playlist/public_html/api/playlist/' + id +'/songs' +"", function(dataSong){           
+            let dataSongs = dataSong.data.songs;
+            var audio = $(playlistConatainer).find($('audio source'));
+            $(audio).attr("src",dataSongs[0].url);
+            var songList = $(playlistConatainer).find($('div.song-list ol'));
+            $(dataSongs).each(function(i,el){
+                var li =  $('<li>').appendTo(songList);
+                $('<span>',{text:"►"}).appendTo(li);
+                $('<button>',{text:el.name, click:function(e){
+                      $(audio).attr("src",el.url); 
+                      console.log(el.url);
+                    }       
+                }).appendTo(li);
+            });
+            console.log(dataSongs);
+        });       
+    });
+}
+
+function deleteAlbumPopup(e,idAlbum){
+    console.log(e);
+    var id = $(e.target).parents('picture').attr("id");
+    var popup =$('<div>',{
+       id:"popup1",
+        click:function(e){
+            if (e.target.id === 'popup1') {
+		this.remove();
+            }
+        }
+    }).appendTo('body');
+    var content = $('<div>',{
+        id:"popup_container1"
+    }).appendTo(popup);
+    $('<h2>',{text:"Are you Sure??"}).appendTo(content);
+    $('<button>',{text:"YES",id:"yesDelete",click:function(e){deleteAlbum(id,popup);}}).appendTo(content);
+    $('<button>',{text:"NO",id:"noDelete",click:function(){$(popup).remove();}}).appendTo(content);
+}
+function deleteAlbum(id,pop){
+    console.log(id);
+    $(pop).remove();
+}
+function editAlbum(){
     
-
-function get(url, className){
-     $.get( url , function(data){
-       var container = $('<div>',{class:className}).appendTo('body');
-       container.html(data);
-    });   
 }
 
- get("albums.html","albums-container");
-   
+function backHome(){
+    $('input.back-button').css({"display":"none"});
+    $('input.search').css({"display":"block"});
+    $('.playlist-container').remove();
+    albums();    
+}
 
 
-$(document).ready(function(){
-        get("playlist.html","playlist-container" );
-     $("button.play-unicode").click(function(e){
-        $(".playlist-container").css({"display":"flex"});
-        $(".albums-container").css({"display":"none"});
-        $('input.back-button').css({"display":"block"});
-        $('input.search').css({"display":"none"});
-        toHomePage();
-    });      
+var newPlaylistObject={};
+$('div.header-add button').click(createPopup);
+
+$('body').keydown(function(e){
+    if (e.keyCode === 27) {
+        $('#popup1').remove();
+    }  
 });
+function createPopup(e){
+   var popup =$('<div>',{
+       id:"popup1",
+        click:function(e){
+            if (e.target.id === 'popup1') {
+		this.remove();
+            }
+        }
+    });
+   $.get('popupAlbums.html',function(data){
+        var content = $('<div>',{
+            id:"popup_container1",
+            html:data
+        }).appendTo(popup);
+        content.find('form').submit(function(e){
+            e.preventDefault();
+            newPlaylistObject.name = $(e.target).find('input[name=name]').val();
+            newPlaylistObject.pic = $(e.target).find('input[name=pic]').val();
+            //console.log(newPlaylistObject);
+            addSongs($(e.target));
+        });
+        $('button#previewButton').click(function(e){
+            e.preventDefault();
+            var picUrl = $(' form input[name=pic]').val();
+           $('img#previewButton').attr("src", ("" + picUrl + "")); 
+        });
 
-function toHomePage(){
-   $('input.back-button').click(function(){
-        $(".playlist-container").css({"display":"none"});
-        $(".albums-container").css({"display":"flex"});
-        $('input.back-button').css({"display":"none"});
-        $('input.search').css({"display":"block"});
-   }); 
+   });      
+    popup.appendTo('body');
+}
+
+function addSongs(form){
+    var content = form.parent().parent();       
+    content.remove();
+    $.get('popupSongs.html',function(data){
+        var newContent = $('<div>',{
+            id:"popup_container",
+            html:data
+        }).appendTo($('#popup1'));
+        for(var i =0; i<3; i++){
+           addSong().prependTo(newContent.find('form'));
+        }
+        newContent.find('button#addSong').click(function(e){
+            e.preventDefault();
+            addSong().insertBefore(newContent.parent().find('#containerButton')); 
+        });
+        newContent.find('form').on("submit", function(e){
+            e.preventDefault();
+            newPlaylistObject.songs=[];
+            $(e.target).find('div.containerInput').each(function(i,el){
+               var song = {};
+               song.url = $(el).find('input.inputUrl').val();
+               song.name =$(el).find('input.inputName').val();
+               newPlaylistObject.songs.push(song);
+            });
+            //console.log(newPlaylistObject);
+            newContent.parent().remove();
+        });
+    });
+    
+    function addSong(){
+        var containerSong = $('<div>',{class:"containerInput"});
+        var labelUrl = $('<label>',{text:"Song URL"}).appendTo(containerSong);
+        $('<input>',{
+            class:"inputUrl",
+            type:"text"
+        }).appendTo(labelUrl);
+        var labelName = $('<label>',{text:"Name"}).appendTo(containerSong);
+        $('<input>',{
+            class:"inputName",
+            type:"text"
+        }).appendTo(labelName); 
+        return containerSong;
+    }
+
 }
 
 
-$('div.header-add button').click(function(){
-    var popup = new popupNewAlbum('http://redipal.diputados.gob.mx/ow_static/themes/origin/images/album_no_cover.png');
-    popup.build();
-    console.log(popup);   
-}); 
+
+
+
+
+
 
 
 
